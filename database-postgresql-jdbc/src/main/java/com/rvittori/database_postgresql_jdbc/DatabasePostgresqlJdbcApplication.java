60d727d945d1ebcaf2c4356abc7f3aa2641efede
Rita Vittori <ritavittori97@gmail.com>
2026-09-02 15:06:49 -0500

    Initial commit

---
+++ database-postgresql-jdbc/src/main/java/com/rvittori/database_postgresql_jdbc/DatabasePostgresqlJdbcApplication.java
@@ -0,0 +1,13 @@
+package com.rvittori.database_postgresql_jdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatabasePostgresqlJdbcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatabasePostgresqlJdbcApplication.class, args);
+	}
+
+}
